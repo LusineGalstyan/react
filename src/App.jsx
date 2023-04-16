609f0e3eb84fce0d3931cb49bbe86005cf7f72c7
LusineGalstyan <126579095+LusineGalstyan@users.noreply.github.com>
2023-04-16 15:06:21 +0400

    homework-69

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,11 @@ class App extends Component {
 
 
 render(){
-<<<<<<< HEAD
     document.title = 'To Do List'
     return (
         
       <div className="App">
         
-=======
-  
-    return (
-      <div className="App">
-
->>>>>>> 250e9a40f138269cdf9bae5e2a1462f7c9ff3bc4
         <Todolist/>
         
 
